cc065ed8706438bd40642a7517a0b49e1bea1bb1
David Rozo <david.rozo@wiedii.co>
2024-09-16 14:45:24 -0500

    295897 - type document endpoint

--- src/src/controllers/parameter/parameter.controller.ts
+++ src/src/controllers/parameter/parameter.controller.ts
@@ -1,7 +1,24 @@
 import { Request, Response } from "express";
+import { HttpResponse } from "../../config/http.response";
+import { TypeDocumentService } from "../../models/parameter/type-document.service";
+
 
 export class ParameterController {
 
-  constructor(){}
+    constructor(
+        private readonly httpResponse: HttpResponse = new HttpResponse(),
+        private readonly typeDocumentService: TypeDocumentService = new TypeDocumentService()
+    ){}
 
+    async getTypesDocument(req: Request, res: Response) {
+        try{
+          const typesDocument = await this.typeDocumentService.getTypesDocument();
+    
+          return this.httpResponse.Ok(res, typesDocument);
+        }catch(error){
+          console.error(error);
+    
+          return this.httpResponse.Error(res);
+        }
+    }
 }
